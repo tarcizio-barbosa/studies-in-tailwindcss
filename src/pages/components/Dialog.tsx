8fe276f5a52fa03cb64c856b2d1c4253ae5d2edc
tarcizio-barbosa <tarcizio.barbosa@outlook.com>
2021-09-23 20:16:17 +0000

    test a open and close modal features

--- src/pages/components/Dialog.tsx
+++ src/pages/components/Dialog.tsx
@@ -1,25 +1,28 @@
 import { useState } from "react"
 import { Dialog } from "@headlessui/react"
 
-export function MyDialog(): JSX.Element {
-  const [isOpen, setIsOpen] = useState(true);
+export function MyDialog({openModal, closeModal}): JSX.Element {
+  // const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)} >
-      <Dialog.Overlay />
+    <Dialog open={openModal} onClose={closeModal} className="fixed z-10 inset-0 overflow-y-auto" >
 
-      <Dialog.Title>Deactivate Account</Dialog.Title>
-      <Dialog.Description>
-        This will permanently deactivate your account.
-      </Dialog.Description>
+      <div className="flex items-center justify-center min-h-screen">
+        <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
+        <div className="relative bg-white rounded max-w-sm mx-auto">
+          <Dialog.Title>Deactivate Account</Dialog.Title>
+          <Dialog.Description>
+            This will permanently deactivate your account.
+          </Dialog.Description>
+          <p>
+            Are you sure you want to deactivate your account? All of your data will
+            be permanently removed. This action cannot be undone.
+          </p>
 
-      <p>
-        Are you sure you want to deactivate your account? All of your data will
-        be permanently removed. This action cannot be undone.
-      </p>
-
-      <button onClick={() => setIsOpen(false)} >Deactivate</button>
-      <button onClick={() => setIsOpen(false)} >Cancel</button>
+          <button onClick={closeModal} >Deactivate</button>
+          <button onClick={closeModal} >Cancel</button>
+        </div>
+      </div>
     </Dialog>
   )
 }
